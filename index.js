2dd8935429f888734f7fb9e37ca241ff55326603
Tuomas <tuomas.2.makinen@tuni.fi>
2023-12-10 23:26:46 +0200

    step 4, 5 & 6

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 
+app.use(express.json())
+
 let persons = [
   { 
     "name": "Arto Hellas", 
@@ -34,16 +36,36 @@ app.get('/info', (req, res) => {
 })
 
 app.get('/api/persons/:id', (req, res) => {
-  const id = req.params.id
-  console.log(id)
-  console.log(persons)
-  const person = persons.find(p => p.id == id)
-  console.log(person)
+  const id = Number(req.params.id)
+  const person = persons.find(p => p.id === id)
   if (person) {
     res.json(person)
   } else {
     res.status(404).end()
   }
+})
+
+app.delete('/api/persons/:id', (req, res) => {
+  const id = Number(req.params.id)
+  persons = persons.filter(p => p.id !== id)
+  res.status(204).end()
+})
+
+app.post('/api/persons', (req, res) => {
+  const newPerson = req.body
+  if (!newPerson.name || !newPerson.number) {
+    return res.status(400).json({ 
+      error: 'name or number missing' 
+    })
+  } else if (persons.find(p => p.name === newPerson.name)) {
+    return res.status(400).json({ 
+      error: 'name must be unique' 
+    })
+  }
+  const id = Math.floor(Math.random() * 10000)
+  newPerson.id = id
+  persons = persons.concat(newPerson)
+  res.json(newPerson) 
 })
 
 const PORT = 3001
